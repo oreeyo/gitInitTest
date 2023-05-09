b331dbea99fe1277e1938c028c3614482ac9e890
oreeyo <oreeyo00@gmail.com>
2023-05-09 21:45:14 +0900

    변경 커밋 테스트

--- test.js
+++ test.js
@@ -4,3 +4,5 @@ let arr = [1, 2, 3, 4];
 let sum = arr.reduce((total, currentValue) => { 
   return total + currentValue; 
 }, 100);
+
+let sum_one = arr.reduce((total, currentValue) => total + currentValue, 100);
