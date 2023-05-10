5f8b7cbc76e3beafa36523aa58ebad2bfa213036
oreeyo <oreeyo00@gmail.com>
2023-05-11 00:34:41 +0900

    터미널에서 커밋해봄

--- test.js
+++ test.js
@@ -9,5 +9,5 @@ let sum_one = arr.reduce((total, currentValue) => total + currentValue, 100);
 //메인은 그대로 진행!
 // 풀 리퀘스트가 전송될거임
 
-console.log("새로운 기능임")
+console.log("터미널로 git 사용중임")
 console.log("충돌 날거임")
